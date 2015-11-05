2e18299dcf1f949553e7f17054e97e1d2534dde4
Jun Aoki <jaoki@apache.org>
2015-11-04 18:42:50 -0800

    added InputFormat idea

--- with-geode/src/main/java/flink_geode/integration/GeodeExecutionEnvironment.java
+++ with-geode/src/main/java/flink_geode/integration/GeodeExecutionEnvironment.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.Plan;
+import org.apache.flink.api.common.PlanExecutor;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 
@@ -15,11 +17,11 @@ import flink_geode.GeodeUtil;
 public class GeodeExecutionEnvironment extends ExecutionEnvironment{
 
 	public DataSet<GeodeRegionEntry> fromRegion(String regionName){
+		// TODO this should not be loaded here and should be lazy evaluation.
 		Cache cache = GeodeUtil.getCache();
 		Region<String, String> region2 = cache.<String, String>getRegion(regionName);
 		Iterator<Entry<String, String>> iterator = region2.entrySet().iterator();
 
-//		Region.Entry<String, String>[] entries = new Region.Entry<String, String>[1];
 		GeodeRegionEntry[] regionEntries = new GeodeRegionEntry[region2.size()];
 		for(int i = 0; iterator.hasNext() ; i++) {
 			Entry<String, String> entry = iterator.next();
@@ -30,39 +32,17 @@ public class GeodeExecutionEnvironment extends ExecutionEnvironment{
 
 		return data;
 		
-
-//		Object[] array = region2.entrySet().toArray();
-//		ArrayList<Map.Entry<String, String>> arrayList = new ArrayList<Map.Entry<String, String>>(region2.entrySet());
-//
-//		arrayList.toArray(new Map.Entry<String, String>[arrayList.size()]));
-//		DataSet<String> text = this.fromElements(arrayList);
-//
-////	    for (Map.Entry<String, String>  entry : region2.entrySet()) {
-////	    	System.out.format("key = %s, value = %s\n", entry.getKey(), entry.getValue());
-////	    }
-//		TypeInformation<Region.Entry<String, String>> type = TypeExtractor.getForObject(region2.getEntry(""));
-//		final ExecutionConfig config = new ExecutionConfig();
-//		//		return new DataSource<T>(this, b, c, regionName);
-////		Arrays.asList(a)
-//		TypeSerializer<Entry<String, String>> serializer = type.createSerializer(config);
-//		CollectionInputFormat<T> inputFormat = new CollectionInputFormat<T>(arrayList, serializer);
-//		return new DataSource<T>(this, inputFormat, type, regionName);
-		
-
-		// ExecutionEnvironment.fromCollection()
-//		data = new X[]
-//		return fromCollection(Arrays.asList(data), TypeExtractor.getForObject(data[0]), Utils.getCallLocationName());
-//		return new DataSource<X>(this, new CollectionInputFormat<X>(data, type.createSerializer(config)), type, callLocationName);
-//		return null;
-		
 	}
 
-//	@Override
-//	public JobExecutionResult execute(String jobName) throws Exception {
-////		this.execute();
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+	@Override
+	public JobExecutionResult execute(String jobName) throws Exception {
+		Plan p = createProgramPlan(jobName);
+		
+		PlanExecutor executor = PlanExecutor.createLocalExecutor(configuration);
+		executor.setPrintStatusDuringExecution(p.getExecutionConfig().isSysoutLoggingEnabled());
+		this.lastJobExecutionResult = executor.executePlan(p);
+		return this.lastJobExecutionResult;
+	}
 
 	@Override
 	public String getExecutionPlan() throws Exception {
