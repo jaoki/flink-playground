35e1d76e3d6bf5f2276f9200cb6736012b44cf26
Jun Aoki <jaoki@apache.org>
2015-11-20 19:33:37 -0800

    added

--- test1/src/test/java/flink_playground/WordCountReadTextFileTest.java
+++ test1/src/test/java/flink_playground/WordCountReadTextFileTest.java
@@ -11,17 +11,17 @@ public class WordCountReadTextFileTest {
 	@Test
 	public void test1() throws Exception {
 
-		final ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
+		final ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();     // [1]
 		String currentDir = System.getProperty("user.dir");
-		DataSet<String> data = env.readTextFile("file:///" + currentDir + "/target/test-classes/text1.txt");
-		DataSet<String> filteredData = data.filter(new FilterFunction<String>(){
-			@Override
-			public boolean filter(String value) throws Exception {
-				return value.startsWith("[ERROR]");
-			}
-		});
-		filteredData.writeAsText("file:///" + currentDir + "/target/output1.txt");
-		env.execute();
+		DataSet<String> data = env.readTextFile("file:///" + currentDir + "/target/test-classes/text1.txt");   // [2]
+		data.filter(new FilterFunction<String>(){                     // [3]
+				@Override
+				public boolean filter(String value) throws Exception {
+					return value.startsWith("[ERROR]");
+				}
+			})
+			.writeAsText("file:///" + currentDir + "/target/output1.txt");   // [4]
+		env.execute();   // [5]
 
 	}
 
